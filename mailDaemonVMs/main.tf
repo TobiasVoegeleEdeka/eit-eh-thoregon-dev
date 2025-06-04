a0792882b4293ebe0a47c0a38cf8f72c79217ba9
Tobias Voegele <tobias.voegele@edeka.de>
2025-06-04 10:48:47 +0200

    postfix vm added

--- mailDaemonVMs/main.tf
+++ mailDaemonVMs/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0" 
+      version = "~> 3.0"
     }
   }
 }
@@ -33,7 +33,7 @@ resource "azurerm_subnet" "vm_subnet" {
 }
 
 resource "azurerm_subnet" "bastion_subnet" {
-  name                 = "AzureBastionSubnet" # Fester Name ist von Azure vorgegeben!
+  name                 = "AzureBastionSubnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.bastion_subnet_prefix]
@@ -46,7 +46,7 @@ resource "azurerm_network_security_group" "vm_nsg" {
   tags                = var.tags
 
   security_rule {
-    name                       = "AllowSSH_IPv4"
+    name                       = "AllowSSHFromAdmin"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
@@ -57,10 +57,9 @@ resource "azurerm_network_security_group" "vm_nsg" {
     destination_address_prefix = "*"
   }
 
-  # Regel für den Zugriff der Ansible-VM
   security_rule {
     name                       = "AllowSSHFromAnsibleVM"
-    priority                   = 101 
+    priority                   = 101
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -69,7 +68,7 @@ resource "azurerm_network_security_group" "vm_nsg" {
     source_address_prefix      = var.allow_ansible_vm_ipv4_cidr
     destination_address_prefix = "*"
   }
-#  VNet und  Azure environment muessen IPv6 unterstuetzen damit das geht 
+
   security_rule {
     name                       = "AllowSSH_IPv6"
     priority                   = 110
@@ -78,23 +77,35 @@ resource "azurerm_network_security_group" "vm_nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = var.allow_ssh_ipv6_cidr 
+    source_address_prefix      = var.allow_ssh_ipv6_cidr
     destination_address_prefix = "*"
   }
 
-  # Spezifischere Werte fuer den Endzustand spaeter
-  # z.B. um Port 5432 nur von der API-VM zur DB-VM zu erlauben.
-  # security_rule {
-  #   name                       = "AllowPostgresFromApiVM"
-  #   priority                   = 200
-  #   direction                  = "Inbound"
-  #   access                     = "Allow"
-  #   protocol                   = "Tcp"
-  #   source_port_range          = "*"
-  #   destination_port_range     = "5432"
-  #   source_address_prefix      = azurerm_network_interface.api_vm_nic.private_ip_address # This only works if API VM has a static private IP
-  #   destination_address_prefix = azurerm_network_interface.db_vm_nic.private_ip_address # This only works if DB VM has a static private IP
-  # }
+  # Neue Regel: Erlaubt eingehenden Mail-Verkehr von anderen Mail-Servern
+  security_rule {
+    name                       = "AllowSMTPInbound"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "25"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  # Neue Regel: Erlaubt Ihrer API, E-Mails zum Versand einzuliefern
+  security_rule {
+    name                       = "AllowSubmissionFromVnet"
+    priority                   = 301
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "587"
+    source_address_prefix      = "VirtualNetwork" # Zugriff nur aus Ihrem VNet
+    destination_address_prefix = "*"
+  }
 }
 
 # --- RESSOURCEN FÜR API-VM ---
@@ -103,8 +114,8 @@ resource "azurerm_public_ip" "api_vm_pip" {
   name                = "${var.api_vm_name}-pip"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  allocation_method   = "Static" # Static is recommended for VMs
-  sku                 = "Standard" # Standard SKU for availability zones and other features
+  allocation_method   = "Static"
+  sku                 = "Standard"
   tags                = var.tags
 }
 
@@ -134,7 +145,6 @@ resource "azurerm_linux_virtual_machine" "api_vm" {
   size                  = var.vm_size
   admin_username        = var.admin_username
   network_interface_ids = [azurerm_network_interface.api_vm_nic.id]
-  #custom_data           = var.api_vm_custom_data_path != null ? base64encode(file(var.api_vm_custom_data_path)) : null
   tags                  = var.tags
 
   admin_ssh_key {
@@ -144,19 +154,14 @@ resource "azurerm_linux_virtual_machine" "api_vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS" # Consider "Premium_LRS" for better performance
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy" # Updated to a more common offer name
+    offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts-gen2"
     version   = "latest"
-  }
-
-  # Enable boot diagnostics for troubleshooting
-  boot_diagnostics {
-    storage_account_uri = null # Set to a storage account URI if you have one for boot diagnostics
   }
 }
 
@@ -197,7 +202,6 @@ resource "azurerm_linux_virtual_machine" "db_vm" {
   size                  = var.vm_size
   admin_username        = var.admin_username
   network_interface_ids = [azurerm_network_interface.db_vm_nic.id]
-  custom_data           = var.db_vm_custom_data_path != null ? base64encode(file(var.db_vm_custom_data_path)) : null
   tags                  = var.tags
 
   admin_ssh_key {
@@ -207,19 +211,71 @@ resource "azurerm_linux_virtual_machine" "db_vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS" # Consider "Premium_LRS" for better performance
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy" # Updated to a more common offer name
+    offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts-gen2"
     version   = "latest"
   }
+}
 
-  # Enable boot diagnostics for troubleshooting
-  boot_diagnostics {
-    storage_account_uri = null # Set to a storage account URI if you have one for boot diagnostics
+# --- RESSOURCEN FÜR POSTFIX-VM ---
+
+resource "azurerm_public_ip" "postfix_vm_pip" {
+  name                = "${var.postfix_vm_name}-pip"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+resource "azurerm_network_interface" "postfix_vm_nic" {
+  name                = "${var.postfix_vm_name}-nic"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tags                = var.tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.postfix_vm_pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "postfix_nic_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.postfix_vm_nic.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "postfix_vm" {
+  name                  = var.postfix_vm_name
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  location              = data.azurerm_resource_group.rg.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  network_interface_ids = [azurerm_network_interface.postfix_vm_nic.id]
+  tags                  = var.tags
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.admin_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
   }
 }
 
@@ -242,4 +298,14 @@ output "db_vm_public_ip" {
 output "db_vm_private_ip" {
   description = "Private IP address of the DB VM."
   value       = azurerm_network_interface.db_vm_nic.private_ip_address
+}
+
+output "postfix_vm_public_ip" {
+  description = "Public IP address of the Postfix VM."
+  value       = azurerm_public_ip.postfix_vm_pip.ip_address
+}
+
+output "postfix_vm_private_ip" {
+  description = "Private IP address of the Postfix VM."
+  value       = azurerm_network_interface.postfix_vm_nic.private_ip_address
 }
--- mailDaemonVMs/variables.tf
+++ mailDaemonVMs/variables.tf
@@ -8,7 +8,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure Region der Ressourcengruppe (wird automatisch aus der RG gelesen, dieser Wert hier ist ein Fallback)."
   type        = string
-  default     = "West Europe" 
+  default     = "West Europe"
 }
 
 # --- NETZWERK ---
@@ -21,25 +21,16 @@ variable "vnet_name" {
 variable "vnet_address_space" {
   description = "Adressraum für das VNet (z.B. [\"10.50.0.0/16\"]). Muss in terraform.tfvars angegeben werden."
   type        = list(string)
-  # Kein Default-Wert, muss in terraform.tfvars angegeben werden
-  # Beispiel für terraform.tfvars:
-  # vnet_address_space = ["10.50.0.0/16"]
 }
 
 variable "vm_subnet_prefix" {
   description = "Adresspräfix für das VM-Subnetz (z.B. \"10.50.1.0/24\"). Muss in terraform.tfvars angegeben werden."
   type        = string
-  # Kein Default-Wert, muss in terraform.tfvars angegeben werden
-  # Beispiel für terraform.tfvars:
-  # vm_subnet_prefix = "10.50.1.0/24"
 }
 
 variable "bastion_subnet_prefix" {
   description = "Adresspräfix für das AzureBastionSubnet (z.B. \"10.50.0.0/26\"). Mindestens /26 empfohlen. Muss in terraform.tfvars angegeben werden."
   type        = string
-  # Kein Default-Wert, muss in terraform.tfvars angegeben werden
-  # Beispiel für terraform.tfvars:
-  # bastion_subnet_prefix = "10.50.0.0/26"
 }
 
 
@@ -53,9 +44,6 @@ variable "admin_username" {
 variable "admin_public_key_path" {
   description = "Pfad zum öffentlichen SSH-Schlüssel für den Admin-Benutzer (z.B. \"~/.ssh/id_rsa.pub\"). Muss in terraform.tfvars angegeben werden."
   type        = string
-  # Kein Default-Wert, muss in terraform.tfvars angegeben werden
-  # Beispiel für terraform.tfvars:
-  # admin_public_key_path = "~/.ssh/id_rsa.pub"
 }
 
 variable "api_vm_name" {
@@ -70,24 +58,16 @@ variable "db_vm_name" {
   default     = "postgres-db-vm"
 }
 
+variable "postfix_vm_name" {
+  description = "Name für die Postfix Mailserver VM."
+  type        = string
+  default     = "postfix-mail-vm"
+}
+
 variable "vm_size" {
   description = "Größe der VMs (z.B. 'Standard_B2s')."
   type        = string
-  default     = "Standard_B1s" 
-}
-
-variable "api_vm_custom_data_path" {
-  description = "Pfad zur Custom Data Datei für die API VM (z.B. \"./mail.api.yaml\"). Auf null setzen, wenn nicht verwendet."
-  type        = string
-  default     = "./mail.api.yaml"
-  nullable    = true
-}
-
-variable "db_vm_custom_data_path" {
-  description = "Pfad zur Custom Data Datei für die DB VM (z.B. \"./mail.db.yaml\"). Auf null setzen, wenn nicht verwendet."
-  type        = string
-  default     = "./mail.api.yaml" 
-  nullable    = true
+  default     = "Standard_B1s"
 }
 
 # --- VARIABLEN FÜR SSH-ZUGRIFF UND TAGS ---
@@ -95,18 +75,18 @@ variable "db_vm_custom_data_path" {
 variable "allow_admin_ipv4_cidr" {
   description = "Administrative IPv4 CIDR für direkten SSH-Zugriff."
   type        = string
-  default     = "91.21.30.141/32" 
+  default     = "91.21.30.141/32"
 }
 
 variable "allow_ansible_vm_ipv4_cidr" {
   description = "Oeffentliche IPv4 CIDR der Ansible Control VM für automatisierten SSH-Zugriff."
   type        = string
-  default = "4.233.100.91/32" 
+  default     = "4.233.100.91/32"
 }
 variable "allow_ssh_ipv6_cidr" {
   description = "IPv6 CIDR-Block für erlaubten SSH-Zugriff. Beispiel: Ihre öffentliche IPv6 /128."
   type        = string
-  default     = "2001:4860:7:1410::f4/128" 
+  default     = "2001:4860:7:1410::f4/128"
 }
 
 variable "tags" {
